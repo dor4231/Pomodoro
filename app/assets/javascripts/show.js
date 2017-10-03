ff10c5fb470d7124e8145b2f346638418b4d16fd
dor4231 <dor4231@gmail.com>
2017-10-03 22:21:58 +0300

    Timer buttons functionality added.

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,27 +1,23 @@
 class Timer {
     constructor(input_seconds=0) {
         this.seconds = parseInt(input_seconds);
-        this.minutes = parseInt(this.seconds / 60);
-        this.hours = parseInt(this.minutes / 60);
     }
 
     time() {
         var timeFormat = ["00", "00", "00"];
-        //Convert the number of seconds to minutes
-        if(this.seconds > 60) {
-            // If needed convert the number of minutes to hours.
-            if(this.minutes > 60) {
-                timeFormat[0] =  this.numberChanger(this.hours);
-                this.minutes -= this.hours * 60;
-            }
-            timeFormat[1] = this.numberChanger(this.minutes);
-            this.seconds -= this.minutes * 60 + this.hours * 3600;
-        }
-        timeFormat[2] = this.numberChanger(this.seconds);
+        var hours = parseInt(this.seconds / 3600);
+        var minutes = parseInt((this.seconds % 3600) / 60);
+        var seconds = parseInt(this.seconds % 60)
+
+        timeFormat[0] = this.numberChanger(hours);
+        timeFormat[1] = this.numberChanger(minutes);
+        timeFormat[2] = this.numberChanger(seconds);
+
         return timeFormat[0] + ":" + timeFormat[1] + ":" + timeFormat[2];
     }
 
-    reset_timer(new_seconds) {
+    set_seconds(new_seconds) {
+        this.seconds = new_seconds;
     }
 
     numberChanger(number) {
@@ -29,31 +25,40 @@ class Timer {
             return number;
         else
             return "0" + number;
-    }   
+    }
 
     start_timer() {
         var self = this;
+        this.pause = false;
         var running_timer = setInterval(function() {
+
             self.print_junk();
-            self.seconds -= 1;
-            self.minutes = parseInt(self.seconds / 60);
-            self.hours = parseInt(self.minutes / 60);
+            self.seconds -= 1
             $("#pom-time").replaceWith('<div id="pom-time">' + self.time() + '</div>')
-            if(self.seconds == 0)
+            if(self.seconds <= 0 || self.pause)
                 clearInterval(running_timer);
         }
             ,1000);
     }
 
+    pause_timer() {
+        this.pause = true;
+    }
+
+    reset_timer() {
+        this.set_seconds(25 * 60);
+        this.pause = true;
+    }    
+
 
     print_junk() {
-        console.log("JUNK!!!!!!!")
+        console.log("Alex & Dor are the BEST!")
     }
    reduce_time() {
     }
 }
 
 
-const myTimer = new Timer(5);
+const myTimer = new Timer(25 * 60);
 
 console.log("seconds: " + myTimer.seconds);
