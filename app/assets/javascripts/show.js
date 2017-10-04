8ceab43d1f348f601e66bd3d094d8636cc86de50
Alex Fore <forealexander4@gmail.com>
2017-10-04 17:13:08 -0500

    updated countDown

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,44 +1,66 @@
 class Timer {
-    constructor(input_seconds=0) {
-        this.seconds = parseInt(input_seconds);
+    constructor(work_seconds, break_seconds) {
+        this.work_seconds = parseInt(work_seconds);
+        this.break_seconds = parseInt(break_seconds);
+        this.mode = "work";
+        this.running = false;
     }
 
-    time() {
+    static time_string(seconds) {
         var timeFormat = ["00", "00", "00"];
-        var hours = parseInt(this.seconds / 3600);
-        var minutes = parseInt((this.seconds % 3600) / 60);
-        var seconds = parseInt(this.seconds % 60)
+        var hours = parseInt(seconds / 3600);
+        var minutes = parseInt((seconds % 3600) / 60);
+        var seconds = parseInt(seconds % 60)
 
-        timeFormat[0] = this.numberChanger(hours);
-        timeFormat[1] = this.numberChanger(minutes);
-        timeFormat[2] = this.numberChanger(seconds);
+        timeFormat[0] = Timer.numberChanger(hours);
+        timeFormat[1] = Timer.numberChanger(minutes);
+        timeFormat[2] = Timer.numberChanger(seconds);
 
         return timeFormat[0] + ":" + timeFormat[1] + ":" + timeFormat[2];
     }
 
     set_seconds(new_seconds) {
-        this.seconds = new_seconds;
+        this.work_seconds = new_seconds;
     }
 
-    numberChanger(number) {
+    static numberChanger(number) {
         if(String(number).length > 1)
             return number;
         else
             return "0" + number;
     }
 
-    start_timer() {
-        var self = this;
-        this.pause = false;
+   countdown(seconds) {
+        this.running = true;
+        var self = this.work_seconds;
+        self.started = true;
         var running_timer = setInterval(function() {
 
             self.print_junk();
             self.seconds -= 1
-            $("#pom-time").replaceWith('<div id="pom-time">' + self.time() + '</div>')
+            $("#pom-time").replaceWith('<div id="pom-time">' + Timer.time_string(self.work_seconds) + '</div>')
             if(self.seconds <= 0 || self.pause)
-                clearInterval(running_timer);
+                if( self.mode == "work" ) {
+                    $("#pom-top").replaceWith('<div id="pom-top" class="break"><h1>Break Time Remaining</h1></div>')
+                    self.mode = "break";
+                    clearInterval(running_timer);
+                    self.countdown(this.work_seconds);
+                } else {
+                    $("#pom-top").replaceWith('<div id="pom-top" class="work"><h1>Work Time Remaining</h1></div>')
+                    self.mode = "work";
+                    clearInterval(running_timer);
+                    self.countdown(this.break_seconds);
+                };
         }
             ,1000);
+    }
+
+    start_timer() {
+        if(!this.running) {
+            this.countdown(this.work_seconds);
+        }
+        
+        this.pause = false;
     }
 
     pause_timer() {
@@ -59,6 +81,13 @@ class Timer {
 }
 
 
-const myTimer = new Timer(25 * 60);
+const myTimer = new Timer(5, 6);
 
 console.log("seconds: " + myTimer.seconds);
+
+
+// starting a session
+
+// Session ends
+
+// A break st
